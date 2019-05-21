861f7303ca1f3d6a5835b59c0024b6c8a57ecbf2
Ramya204 <ramyapri.20@gmail.com>
2019-05-21 15:36:20 +0530

    Ramya added a method

--- src/org/git/GitProgram.java
+++ src/org/git/GitProgram.java
@@ -1,6 +1,9 @@
 package org.git;
 
 public class GitProgram {
+	public static void ramya(){
+		System.out.println("Ramya Added a method");
+	}
 	public static void main(String[] args) {
 		System.out.println("Git");
 		System.out.println("Done");
