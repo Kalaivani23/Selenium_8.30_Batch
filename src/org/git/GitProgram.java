251dfa9f6bd4bcc0c737d31c094f257363ddc947
chavalisaitharun <chavalisaitharun@gmail.com>
2019-05-21 20:25:37 +0530

    added new code

--- src/org/git/GitProgram.java
+++ src/org/git/GitProgram.java
@@ -1,9 +1,16 @@
 package org.git;
 
+import java.util.Scanner;
+
 public class GitProgram {
 	public static void main(String[] args) {
 		System.out.println("Git");
 		System.out.println("Done");
 		System.out.println("Nivetha added");
+		System.out.println("Adding code");
+		System.out.println("Enter line 1");
+		Scanner s=new Scanner(System.in);
+		String st=s.next();
+		System.out.println(st);
 	}
 }
