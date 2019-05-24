c88e8ad7c5c5674dac02f2f49446a280c5a7f1cd
balajidamodharan <trybala29@gmail.com>
2019-05-24 20:29:48 +0530

    me added new method

--- src/org/git/GitProgram.java
+++ src/org/git/GitProgram.java
@@ -1,6 +1,10 @@
 package org.git;
 
 public class GitProgram {
+	public void github() {
+		System.out.println("GitHub");
+
+	}
 	public static void main(String[] args) {
 		System.out.println("Git");
 		System.out.println("Done");
@@ -12,5 +16,6 @@ public class GitProgram {
 		System.out.println();
 		System.out.println("Balaji added");
 System.out.println("Balaji adding Second line of code");
+System.out.println("balaji added a new method");
 	}
 }
